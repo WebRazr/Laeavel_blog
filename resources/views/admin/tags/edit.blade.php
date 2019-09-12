d94c1d9b0d027c476d43dfdf98217c2f72324610
John Doe <johndoe@example.com>
2019-09-12 18:11:20 +0300

    first commit

---
+++ resources/views/admin/tags/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.layout')
+
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Добавить тег
+            <small>приятные слова..</small>
+        </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Меняем тег</h3>
+            </div>
+            <div class="box-body">
+                {{Form::open(['route'=>['tags.update',$tag->id], 'method'=>'put'])}}
+                <div class="col-md-6">
+
+                    @include('admin.errors')
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Название</label>
+                        <input type="text" class="form-control"  id="exampleInputEmail1"
+                          name="title"     placeholder="" value="{{$tag->title}}">
+                    </div>
+                </div>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+                <submit class="btn btn-default">
+                    <a href="{{route('tags.index')}}">назад</a>
+                </submit>
+                <button class="btn btn-warning pull-right">Изменить</button>
+            </div>
+            <!-- /.box-footer-->
+        </div>
+        {{Form::close()}}
+        <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+    @endsection
